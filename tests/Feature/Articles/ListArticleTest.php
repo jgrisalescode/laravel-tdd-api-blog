a383e363765306142630af68e3de6005fe5840f1
Julian Grisales <jgrisales@gmail.com>
2022-11-06 12:15:46 +0100

    GET Article

---
+++ tests/Feature/Articles/ListArticleTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature\Articles;
+
+use App\Models\Article;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ListArticleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_fetch_a_single_article()
+    {
+        $this->withoutExceptionHandling();
+        $article = Article::factory()->create();
+        $response = $this->getJson(route('api.v1.articles-show', $article));
+        $response->assertExactJson([
+            "data" => [
+                "type" => "articles",
+                "title" => (string) $article->getRouteKey(),
+                "attributes" => [
+                    "title" => $article->title,
+                    "slug"  => $article->slug,
+                    "content"   => $article->content
+                ],
+                "links" => [
+                    "self" => route("api.v1.articles-show", $article)
+                ]
+            ]
+        ]);
+    }
+}
